e99dab0541861dc23be17882d85fc16f2cd4768e
Abdikhaniyev <alisher147741@gmail.com>
2023-10-30 19:58:54 +0600

    feat: change basic layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { Component } from 'react';
 import './App.css';
+import Banner from './components/Banner';
+import Characters from './components/Characters';
 
-function App() {
-  return <></>;
+interface Props {}
+
+interface State {
+  search: string;
+}
+
+export class App extends Component<Props, State> {
+  state = {
+    search: localStorage.getItem('search') || '',
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    localStorage.setItem('search', event.target.value);
+    this.setState({ search: event.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <Banner search={this.state.search} setSearch={this.handleChange} />
+        <Characters search={this.state.search} />
+      </>
+    );
+  }
 }
 
 export default App;
