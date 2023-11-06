92e9cd8f118e28e64092997ddc9e2df9b3ee3815
Abdikhaniyev <alisher147741@gmail.com>
2023-11-06 21:05:51 +0600

    style: redesign banner component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,14 +1,59 @@
-import Header from '../Header';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
+import { getCharacter } from '../../api/character';
+import portal from '../../assets/portal.png';
+import { Character } from '../../interfaces';
+import CharacterCard from '../CharacterCard';
 import styles from './Banner.module.scss';
 
-interface Props {
-  setSearch: (value: string) => void;
-}
+export default function Banner() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [character, setCharacter] = useState<Character | undefined>(undefined);
 
-export default function Banner({ setSearch }: Props) {
+  const fetchCharacter = async (id: string) => {
+    setLoading(true);
+    getCharacter(id)
+      .then((data) => {
+        setCharacter(data);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchCharacter('1');
+  }, []);
+
+  const onMouseMove = (event: MouseEvent) => {
+    const { pageX, pageY } = event;
+    const x = (window.innerWidth / 2 - pageX) / 32;
+    const y = (window.innerHeight / 2 - pageY) / 20 + 15;
+
+    requestAnimationFrame(() => {
+      if (ref.current) {
+        const transform = `rotateX(${y}deg) rotateY(${x}deg) rotateZ(-9deg)`;
+        ref.current.style.transform = transform;
+      }
+    });
+  };
+
+  const onMouseLeave = () => {
+    requestAnimationFrame(() => {
+      if (ref.current) {
+        const transform = `rotateX(16deg) rotateY(0deg) rotateZ(-9deg)`;
+        ref.current.style.transform = transform;
+      }
+    });
+  };
+
   return (
-    <div className={styles.banner}>
-      <Header setSearch={setSearch} />
+    <div className={styles.banner} onMouseMove={onMouseMove} onMouseLeave={onMouseLeave}>
+      <div className={styles['banner-left']}>Rick and Morty</div>
+      <div className={styles['banner-right']}>
+        {!loading && character && <CharacterCard ref={ref} bannerCard character={character} />}
+        <img className={styles.portal} src={portal} alt="portal" />
+      </div>
     </div>
   );
 }
