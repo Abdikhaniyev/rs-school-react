ed1dd271f897461654647c8069c2055ac60ba2ba
Abdikhaniyev <alisher147741@gmail.com>
2023-11-07 21:01:46 +0600

    fix: episode api

--- src/api/episode.ts
+++ src/api/episode.ts
@@ -1,6 +1,9 @@
 const EPISODES_ENDPOINT = `${import.meta.env.VITE_API_HOST}/episode`;
 
-const getEpisodes = async (episodes: (string | undefined)[]) => {
+const getEpisodes = async (episodes: string[] | undefined) => {
+  if (!episodes) {
+    return [];
+  }
   const ids = episodes?.map((episode) => episode?.split('/').pop()).join(',');
   const response = await fetch(`${EPISODES_ENDPOINT}/${ids}`);
   const data = await response.json();
