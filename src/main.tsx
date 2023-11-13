f4ad6f12933b6f3e6baa047516531d4ad06134c7
Abdikhaniyev <alisher147741@gmail.com>
2023-11-13 21:42:50 +0600

    feat: add and configure RTK

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
 import { StoreProvider } from './context/StoreContext';
 import './index.scss';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <StoreProvider>
-        <Router />
-      </StoreProvider>
+      <Provider store={store}>
+        <StoreProvider>
+          <Router />
+        </StoreProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
